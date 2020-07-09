4a0dbd72e56632b90e8c1901f1ee6d24ebffd7e1
Sashapet <fastzumftwlols@gmail.com>
2020-07-09 17:03:42 +0300

    Updates

---
+++ precache-manifest.0fe1cec590dcfe81cafad9f8c2c1ab33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08da2eabe0c254d78de74f3c9b7ad557",
+    "url": "/GoogleLiveBookSearchApiWithHooks/index.html"
+  },
+  {
+    "revision": "667be8ba7433358cbe74",
+    "url": "/GoogleLiveBookSearchApiWithHooks/static/css/main.2f6fde76.chunk.css"
+  },
+  {
+    "revision": "72ee904e4902677bc02c",
+    "url": "/GoogleLiveBookSearchApiWithHooks/static/js/2.7c73810f.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/GoogleLiveBookSearchApiWithHooks/static/js/2.7c73810f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667be8ba7433358cbe74",
+    "url": "/GoogleLiveBookSearchApiWithHooks/static/js/main.b7ecfc20.chunk.js"
+  },
+  {
+    "revision": "4d072ed84b6fcf3df8b2",
+    "url": "/GoogleLiveBookSearchApiWithHooks/static/js/runtime-main.ee36a7ab.js"
+  }
+]);
